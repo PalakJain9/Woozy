09f9676c656a416d1c621d9ef2a56743c6bf9969
PalakJain9 <palakb188@gmail.com>
2022-06-26 21:33:56 +0530

    temporary changes

--- background.js
+++ background.js
@@ -1,10 +1,39 @@
 console.log("hello");
 
-chrome.action.onClicked.addListener(buttonClicked);
+/*chrome.action.onClicked.addListener(buttonClicked);
 function buttonClicked(){
     console.log("button clicked");
-    /*chrome.windows.create({
+    chrome.windows.create({
         url: chrome.runtime.getURL("popup.html"),
         type: "popup"
-    }); */
+    }); 
 }
+
+let isBtnClicked = false;
+let timer = 0;
+let default_msg = document.getElementById("default_msg");
+let reminder_msg = document.getElementById("reminder");
+let btn_setReminder = document.getElementById("start");
+btn_setReminder.addEventListener('click', updateValue());
+
+const updateValue = () => {
+    isBtnClicked = true;
+}
+
+const getValue = (val) => {
+    let time = val.value;
+    if(isBtnClicked == true) runTimer(time);
+}
+
+const runTimer = (time) => {
+    if(timer == time) displayReminder();
+    ++timer;
+    console.log(timer);
+} 
+setTimeout(runTimer,60000);
+
+const displayReminder = () => {
+    default_msg.style.display = "none";
+}
+*/
+
